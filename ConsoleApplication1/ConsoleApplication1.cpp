67c70736d3f1449d913c7238e52217944cf5516f
c0admin <mustafagldren@gmail.com>
2023-01-13 01:30:58 +0300

    Proje dosyası ekle.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,35 @@
+﻿/*istenilen iki sayinin küçüğünü bulup tek mi çift mi olduğunu bulma*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int sayi1, sayi2;
+
+    cout << "ilk sayi degerini giriniz :" << endl;
+    cin >> sayi1;
+    cout << "ikinci sayi degerini giriniz :" << endl;
+    cin >> sayi2;
+
+    if (sayi1 > sayi2) {
+        if (sayi2 % 2 == 0) {
+            cout << "kucuk sayi cifttir." << endl;
+       }
+        else {
+            cout << "kucuk sayi tektir." << endl;
+        }
+    }
+    else {
+        if (sayi1 % 2 == 0) {
+            cout << "kucuk sayi cifttir." << endl;
+        }
+        else {
+            cout << "kucuk sayi tektir." << endl;
+        }
+    }
+        
+
+system("pause");
+return 0;
+}
